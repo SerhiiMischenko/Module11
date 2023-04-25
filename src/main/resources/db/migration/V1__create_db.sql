f0d3204ffda9cdc9991ba5e171b4efafe2f6eb0a
SerhiiMischenko <serj.roshen@gmail.com>
2023-04-25 22:38:22 +0300

    First commit

---
+++ src/main/resources/db/migration/V1__create_db.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE IF NOT EXISTS SpaceTravel_DB;
+
+USE SpaceTravel_DB;
+
+CREATE TABLE Client (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(200) NOT NULL CHECK (LENGTH(name) >= 3 AND LENGTH(name) <= 200),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE Planet (
+  id VARCHAR(50) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
+  name VARCHAR(500) NOT NULL CHECK (LENGTH(name) >= 1 AND LENGTH(name) <= 500 AND name REGEXP '^[A-Z0-9]+$'),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE Ticket (
+  id INT NOT NULL AUTO_INCREMENT,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  client_id INT NOT NULL,
+  from_planet_id VARCHAR(50) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
+  to_planet_id VARCHAR(50) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (client_id) REFERENCES Client(id),
+  FOREIGN KEY (from_planet_id) REFERENCES Planet(id),
+  FOREIGN KEY (to_planet_id) REFERENCES Planet(id)
+);}
---
+++ src/main/resources/db/migration/V2__populate_db.sql
@@ -0,0 +1,32 @@
+INSERT INTO Client (name) VALUES
+  ('John Smith'),
+  ('Mary Johnson'),
+  ('Robert Lee'),
+  ('Emily Davis'),
+  ('William Brown'),
+  ('Sarah Jackson'),
+  ('Michael Wilson'),
+  ('Jessica Garcia'),
+  ('David Rodriguez'),
+  ('Amanda Hernandez');
+
+INSERT INTO Planet (id, name) VALUES
+  ('EARTH', 'Earth'),
+  ('MARS', 'Mars'),
+  ('VENUS', 'Venus'),
+  ('JUPITER', 'Jupiter'),
+  ('SATURN', 'Saturn');
+
+INSERT INTO Ticket (client_id, from_planet_id, to_planet_id)
+VALUES
+  (1, 'EARTH', 'MARS'),
+  (2, 'MARS', 'EARTH'),
+  (3, 'EARTH', 'VENUS'),
+  (4, 'VENUS', 'JUPITER'),
+  (5, 'JUPITER', 'SATURN'),
+  (6, 'SATURN', 'EARTH'),
+  (7, 'MARS', 'VENUS'),
+  (8, 'VENUS', 'MARS'),
+  (9, 'JUPITER', 'MARS'),
+  (10, 'SATURN', 'VENUS');
+}
