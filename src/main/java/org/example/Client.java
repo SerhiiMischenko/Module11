941e89136e9d58c91c5b00ff03672311cc7f1ba8
SerhiiMischenko <serj.roshen@gmail.com>
2023-04-26 14:50:19 +0300

    Added Client.java, Planet.java classes. And hibernate.cfg.xml file

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Client")
+public class Client {
+}
---
+++ src/main/java/org/example/Planet.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Planet")
+public class Planet {
+
+}
