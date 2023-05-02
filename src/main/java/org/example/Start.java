a0328d0692a6cbf4a7e570c94c22e2c0a6226f76
SerhiiMischenko <serj.roshen@gmail.com>
2023-05-02 14:12:17 +0300

    Edit configuration Start.java class

--- src/main/java/org/example/Start.java
+++ src/main/java/org/example/Start.java
@@ -6,7 +6,7 @@ public class Start {
 
         public static void main(String[] args) {
             ClientCrudService clientCrudService = new ClientCrudService();
-            clientCrudService.getClient(1L);
+            System.out.println(clientCrudService.getClient(1L));
             clientCrudService.createClient("Serhii Mischenko");
             clientCrudService.updateClient(1L, "John Snow");
             clientCrudService.deleteClient(17L);
@@ -15,6 +15,5 @@ public class Start {
             System.out.println(planetCrudService.getPlanet(null));
             planetCrudService.updatePlanet("CENTAVRA", "AlphaCentavra");
             planetCrudService.deletePlanet("CENTAVRA");
-
         }
 }
